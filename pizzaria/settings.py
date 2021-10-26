6e71d8b4e9aa901ffe938b0d20e529bb00b89978
Gabriel <gabrieldeborba07@gmail.com>
2021-10-26 18:29:03 -0300

    Adding secret_django_key

--- pizzaria/settings.py
+++ pizzaria/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 from django.contrib.messages import constants
-from . import secret_datas
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = secret_datas.secret_key
+SECRET_KEY = 'r$*graazto85got^siee2*0e*zdv1ht!0j0lw$9+ze2&b^gq@z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
